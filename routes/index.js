9dd8a4b9bf3a6fb49fa9a772b98b06030501b09d
roba <10408936+roba-adnew@users.noreply.github.com>
2024-04-15 15:53:22 -0400

    Set-up express project and added initial content

---
+++ routes/index.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const router = express.Router();
+
+const messages = [
+  {
+    text: "Hi there!",
+    user: "Amando",
+    added: new Date()
+  },
+  {
+    text: "Hello World!",
+    user: "Charles",
+    added: new Date()
+  }
+];
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.locals.messages = messages;
+  res.render('index', { title: "Mini Message Board" });
+});
+
+module.exports = router;
