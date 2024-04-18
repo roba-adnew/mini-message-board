3b53dfdf4e225f8f30d209bfa89f7bad17de3ea5
roba <10408936+roba-adnew@users.noreply.github.com>
2024-04-18 13:06:07 -0400

    Project completed

--- routes/index.js
+++ routes/index.js
@@ -15,9 +15,23 @@ const messages = [
 ];
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.locals.messages = messages;
-  res.render('index', { title: "Mini Message Board" });
+router.get('/', function (req, res, next) {
+  res.render('index', { title: "Mini Message Board", messages: messages });
 });
+
+router.get('/new', function (req, res) {
+  res.render('form');
+})
+
+router.post('/new', function (req, res) {
+  messages.push(
+    {
+      text: req.body.message,
+      user: req.body.name,
+      added: new Date()
+    }
+  )
+  res.redirect('/')
+})
 
 module.exports = router;
